78ba7c42a27c048902b310af963faf016a1d2ca3
yuruofeifei <yuruofeifei@gmail.com>
2018-12-05 18:37:03 -0800

    Fix indentation

--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -28,20 +28,20 @@ type keyValueServer struct {
     client_request_buffer chan *client_request
     close_server_chan chan int
     close_client_chan chan int
-	active_count_chan chan int
+    active_count_chan chan int
     dropped_count_chan chan int
 }
 
 type client struct {
-	c_id int
-	conn net.Conn
-	dead chan int
-	response_buffer chan []byte
+    c_id int
+    conn net.Conn
+    dead chan int
+    response_buffer chan []byte
 }
 
 type client_request struct {
-	cli *client
-	msg []string
+    cli *client
+    msg []string
 }
 
 // New creates and returns (but does not start) a new KeyValueServer.
@@ -104,56 +104,55 @@ func start_server(kvs *keyValueServer) {
     kvs.clients_list_chan <- make(map[int]*client)
     kvs.active_count_chan <- 0
     kvs.dropped_count_chan <- 0
-	for {
-		select {
-			case <- kvs.close_server_chan:
-				clients_list := <- kvs.clients_list_chan
-				for c_id := range clients_list {
-					clients_list[c_id].conn.Close()
-				}
-				kvs.clients_list_chan <- nil
-				kvs.ln.Close()
-				return 
-			case c_id := <- kvs.close_client_chan:
-				clients_list := <- kvs.clients_list_chan
-				client, ok := clients_list[c_id]
-				delete(clients_list, c_id)
-				kvs.clients_list_chan <- clients_list
-				if ok {
-					client.dead <- 1
-					client.conn.Close()
-					active_count := <- kvs.active_count_chan
-					kvs.active_count_chan <- active_count - 1
-					dropped_count := <- kvs.dropped_count_chan 
-					kvs.dropped_count_chan <- dropped_count + 1
-				}
-			case cli_request := <- kvs.client_request_buffer: 
-				key := cli_request.msg[1]
-				switch cli_request.msg[0] {
-					case PUT: 
-			        	put(key, []byte(cli_request.msg[2]))
-		            case GET:
-		                send_to_client(cli_request.cli, key, get(key))
-		            case DEL:
-		                clear(key)
-		        }
+    for {
+	select {
+	    case <- kvs.close_server_chan:
+		clients_list := <- kvs.clients_list_chan
+		for c_id := range clients_list {
+		    clients_list[c_id].conn.Close()
+		}
+		kvs.clients_list_chan <- nil
+		kvs.ln.Close()
+		return 
+	    case c_id := <- kvs.close_client_chan:
+		clients_list := <- kvs.clients_list_chan
+		client, ok := clients_list[c_id]
+		delete(clients_list, c_id)
+		kvs.clients_list_chan <- clients_list
+		if ok {
+	            client.dead <- 1
+		    client.conn.Close()
+		    active_count := <- kvs.active_count_chan
+		    kvs.active_count_chan <- active_count - 1
+		    dropped_count := <- kvs.dropped_count_chan 
+		    kvs.dropped_count_chan <- dropped_count + 1
+		}
+	    case cli_request := <- kvs.client_request_buffer: 
+		key := cli_request.msg[1]
+		switch cli_request.msg[0] {
+		    case PUT: 
+			put(key, []byte(cli_request.msg[2]))
+		    case GET:
+			send_to_client(cli_request.cli, key, get(key))
+		    case DEL:
+			clear(key)
 		}
 	}
-	
+    }
 }
 
 func send_to_client(cli *client, key string, values []([]byte)) {
-	for _, v := range values {
-		select {
-			case cli.response_buffer <- []byte(fmt.Sprintf("%s,%s\n", key, string(v))):
-		    default:
-		        fmt.Println("Client response buffer reach limit!")
-		}
+    for _, v := range values {
+	select {
+	    case cli.response_buffer <- []byte(fmt.Sprintf("%s,%s\n", key, string(v))):
+	    default:
+	        fmt.Println("Client response buffer reach limit!")
 	}
+    }
 }
 
 func join_client(kvs *keyValueServer) {
-	client_id := 0
+    client_id := 0
     for {
         conn, err := kvs.ln.Accept()
         if err != nil {
@@ -179,34 +178,31 @@ func join_client(kvs *keyValueServer) {
 func process_client_read(kvs *keyValueServer, cli *client) {
     for {
     	select {
-    		case <- cli.dead:
-    			return
-	    	default:
-	    		msg := <- cli.response_buffer
-				cli.conn.Write(msg)
-    	}
-    	
+    	    case <- cli.dead:
+    		return
+            default:
+	    	msg := <- cli.response_buffer
+		cli.conn.Write(msg)
+	}
     }
 }
 
 func process_client_write(kvs *keyValueServer, cli *client) {
-	reader := bufio.NewReader(cli.conn)
+    reader := bufio.NewReader(cli.conn)
     for {
     	select {
-    		case <- cli.dead:
-    			return
-    		default:
-    			message, err := reader.ReadString('\n')
-		        if err == io.EOF {
-					kvs.close_client_chan <- cli.c_id
-		        } else if err != nil {
-		        	return
-		        } else {
-
-					message = strings.TrimSuffix(message, "\n")
-				    kvs.client_request_buffer <- &client_request{cli: cli, msg: strings.Split(message, ",")}
-				}
+   	    case <- cli.dead:
+    		return
+      	    default:
+    		message, err := reader.ReadString('\n')
+	        if err == io.EOF {
+		    kvs.close_client_chan <- cli.c_id
+	        } else if err != nil {
+		    return
+                } else {
+		    message = strings.TrimSuffix(message, "\n")
+		    kvs.client_request_buffer <- &client_request{cli: cli, msg: strings.Split(message, ",")}
+		}
     	}
-    	
     }
 }
